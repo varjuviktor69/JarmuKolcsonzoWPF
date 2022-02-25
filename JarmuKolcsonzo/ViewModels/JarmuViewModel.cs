3ed7311cb9b88fc97281e01afcd63b8754750a2e
Viktor Crow <varjuviktor69@gmail.com>
2022-02-25 09:04:57 +0100

    ViewModels added

---
+++ JarmuKolcsonzo/ViewModels/JarmuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarmuKolcsonzo.ViewModels
+{
+    public class JarmuViewModel : ObservableObject
+    {
+    }
+}
---
+++ JarmuKolcsonzo/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarmuKolcsonzo.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private ObservableObject _selectedViewModel;
+
+        public ObservableObject SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            // Értesíti a view rész, hogy változás történt az objektumban
+            set { SetProperty(ref _selectedViewModel, value); }
+        }
+        
+        // Frissíti a ContentControllban a megjelenítendő ablakot
+        public RelayCommand<string> UpdateViewCommand { get; set; }
+
+        private void UpdateView(string windowName)
+        {
+            if (windowName == "jármű")
+            {
+                SelectedViewModel = new JarmuViewModel();
+            }
+            else
+            {
+                SelectedViewModel = new UgyfelViewModel();                   
+            }
+        }
+
+        private ObservableObject JarmuViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new RelayCommand<string>(e => UpdateView(e));
+        }
+    }
+}
